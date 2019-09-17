5c7f4ca60830cbf62ae865fc74e35ea7cd25924d
Thomas Lee <thomaslee@yam.ai>
2019-09-18 02:07:07 +0800

    Set settings to default values when they are not given

--- web.py
+++ web.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# Copyright 2019 YAM AI Machinery Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from flask import Flask, render_template, request, redirect, url_for, \
     send_from_directory
 import requests as http_client
@@ -29,6 +45,8 @@ def sort_dict(dct):
 
 
 def create_app(classifier_endpoint):
+    if not classifier_endpoint:
+        classifier_endpoint = ENDPOINT
     app = Flask(__name__, static_url_path='/static')
     app.logger.info('Classifier endpoint = {}'.format(classifier_endpoint))
 
@@ -110,12 +128,12 @@ def main(argv):
         host, port = host_port(HOST_PORT)
         opts, _ = getopt.getopt(argv[1:], 'c:h:')
         for opt, arg in opts:
-            if opt == '-c':
+            if opt == '-c' and arg:
                 classifier_endpoint = arg
                 if not validate_uri(classifier_endpoint):
                     quit(progname, Exception(
                         'Invalid endpoint: {}'.format(classifier_endpoint)))
-            elif opt == '-h':
+            elif opt == '-h' and arg:
                 host, port = host_port(arg)
         app = create_app(classifier_endpoint)
         app.run(host=host, port=port)
